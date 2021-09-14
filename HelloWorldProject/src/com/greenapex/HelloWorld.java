272e5fb41caf86a6e0bcb9df0091a438f8f815b6
nazneenbee <beenazneen@gmail.com>
2021-09-14 17:00:57 +0530

    first commit

---
+++ HelloWorldProject/src/com/greenapex/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.greenapex;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      System.out.println("Hello World");
+      System.out.println("Welcome in Java");
+	}
+
+}
