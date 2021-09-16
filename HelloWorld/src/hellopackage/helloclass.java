e02da338d8c23ca75bf11ac534cbfe95581d31b2
nazneenbee <beenazneen@gmail.com>
2021-09-16 17:48:38 +0530

    add the new msg

--- HelloWorld/src/hellopackage/helloclass.java
+++ HelloWorld/src/hellopackage/helloclass.java
@@ -9,6 +9,11 @@ public class helloclass {
 		System.out.println("Welcome to git again");
 		System.out.println("Welcome to git again try");
 		System.out.println("Welcome");
+		System.out.println("Welcome");
+		System.out.println("Welcome to jenkins");
+		System.out.println("Welcome to git again");
+		System.out.println("Welcome to git again try");
+		System.out.println("Welcome");
 
 	}
 
